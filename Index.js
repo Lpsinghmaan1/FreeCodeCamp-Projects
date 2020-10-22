0251e916cad536cdf40316c3b7d83708396636a7
LP <lpsinghmaan1@gmail.com>
2020-10-23 09:56:40 +1000

    Class commit

---
+++ Index.js
@@ -0,0 +1,12 @@
+console.log ('hello world');
+
+class People {
+    constructor(name,age) {   
+    this.name = name;
+    this.age = age;
+}
+}
+
+const Jass = new People ('jass',22)
+
+console.log(Jass.name,Jass.age)

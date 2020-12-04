4fd79a37570c93fa15bf721f0cc1817573d4ccbc
LP <lpsinghmaan1@gmail.com>
2020-12-04 11:10:45 +1000

    4/12/20

--- Index.js
+++ Index.js
@@ -1,3 +1,4 @@
+/*
 console.log ('hello world');
 // this is my first comment
 
@@ -49,7 +50,7 @@ person.age = 33;
 
 console.log(person.age);
 
-// object oonstructor 
+// object Constructor 
 
 function Animal (){};
 function Birds (){};
@@ -82,7 +83,7 @@ Birds.prototype = Object.create(Animal.prototype);
 let lion = new Animal;
 let crow = new Birds;
 
-console.log(crow.eat);
+console.log(crow.eat());
 console.log(crow.livesIn);
 console.log(lion.eat);
 
@@ -123,6 +124,106 @@ motionModule.glideMixin(crow);
 
 
 console.log(crow.glide());
+
+let crow2 = new Birds;
+
+//console.log(crow2.glide());
+*/
+
+let array = [1,2,3,4,5,6,7]
+console.log(array);
+
+let test = [1,2,[3,4],[5,6]]
+console.log(test);
+
+console.log(test[3][1]);
+
+//push - modifies the array directly
+array.push(8);
+console.log(array);
+
+//pop - modifies the array directly
+array.pop(8);
+console.log(array);
+
+//spead
+let newArray = [...array];
+console.log(newArray);
+
+let animalArray= [ {
+  live:"pageYOffset",
+  jungle: true
+},
+{
+  live:"pageYOffset",
+  jungle: true
+}
+]
+
+let newAnimal = [...animalArray];
+console.log(newAnimal);
+
+animalArray[0].jungle = false;
+console.log(newAnimal);
+
+//spread join
+let secondArray = [...array,...newArray];
+console.log(secondArray);
+
+//indexOf
+console.log(secondArray.indexOf("a"));
+console.log(secondArray.indexOf(1));
+
+//slice - returns a new array
+var carParts = ["lights","tyer","engine","windows","body"];
+console.log(carParts.slice(1,3));
+
+//toString - returns a new array
+let listOfCarParts = carParts.toString();
+console.log(listOfCarParts);
+
+//Filter - returns a new array
+let listOfCarParts2 = carParts.filter(function(hasThis){
+  return hasThis === "hat";
+})
+console.log(listOfCarParts2);
+
+//Join
+console.log(array.join(''));
+
+//Splice
+console.log(array.splice(1,2,"5"));
+console.log(array);
+
+//concat
+var allParts = listOfCarParts.concat(listOfCarParts2);
+console.log(allParts);
+
+//Shift - modifies the original array directly
+let shifterAllparts = array.shift();
+console.log(shifterAllparts);
+console.log(array);
+
+//map - returns a new array
+let a = carParts.map(function(part){
+  return part.toUpperCase();
+})
+
+console.log(a);
+
+//sort - original arrary
+console.log(secondArray.sort());
+
+//every
+function one(value){
+  return value > 25;
+}
+console.log(secondArray.every(one));
+
+
+
+
+
 
 
 
